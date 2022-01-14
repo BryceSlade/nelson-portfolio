03cf76bb03b94158cf27136f4b664b779bd1d33b
Bryce Slade <bryceslade@gmail.com>
2022-01-14 13:36:56 -0700

    header completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from "./components/about/About"
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 import './app.css'
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <About/>
       <Skills/>
       <Contact/>
-      {/* <Footer/> */}
     </div>
   );
 }

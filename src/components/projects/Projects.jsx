faab5dbf4ebb9dccc16a6bb51d522fae12c4fe06
Bryce Slade <bryceslade@gmail.com>
2022-01-27 10:24:45 -0700

    Projects component built out - for now

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,21 @@
+import Project from '../project/Project';
+import './projects.css';
+import { projects } from '../../data';
+
+const Projects = () => {
+    return (
+        <div className='pl'>
+            <div className="pl-texts">
+                <h1 className='pl-title'>Projects</h1>
+                <p className='pl-desc'>Take a look at some of the projects I've worked on. More to come!</p>
+            </div>
+            <div className="pl-list">
+                {projects.map(item => (
+                    <Project key={item.id} img={item.img} link={item.link}/>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Projects;

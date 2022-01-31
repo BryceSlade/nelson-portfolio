605d4a119ac75e3f8145203a9e0776e40a0452e7
Bryce Slade <bryceslade@gmail.com>
2022-01-31 12:53:29 -0700

    website fully responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,13 @@
-import './contact.css'
-import phone from '../../img/phone.png'
-import email from '../../img/email.png'
-import address from '../../img/address.png'
-import { useRef } from 'react'
+import './contact.css';
+import phone from '../../img/phone.png';
+import email from '../../img/email.png';
+import address from '../../img/address.png';
+import { useRef } from 'react';
 import emailjs from 'emailjs-com';
 import { useState } from 'react';
-import { Grid, Paper, TextField, Button } from '@material-ui/core';
 
 const Contact = () => {
     const formRef = useRef()
-    const [inputs, setInputs] = useState('')
     const [sent, setSent] = useState(false)
 
     const handleSubmit = event => {
@@ -18,7 +16,6 @@ const Contact = () => {
         .then((result) => {
             console.log(result.text);
             setSent(true)
-            setInputs('')
         }, (error) => {
             console.log(error.text);
         });
@@ -80,82 +77,10 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
-
-                    {/* <div className="c-right">
-                        <Grid>
-                            <Paper elevation={24} style={paperStyle} className='paper'>
-                                <p className="c-desc">
-                                    <b>Interested?</b> Send me an email!
-                                </p>
-                                <form ref={formRef} onSubmit={handleSubmit}>
-                                    <TextField
-                                        style={textboxStyle}
-                                        fullWidth
-                                        variant='outlined'
-                                        label='name'
-                                        // value={inputs.name}
-                                        // onChange={(e)=>setInputs(e.target.inputs)}
-                                        type='text'
-                                        placeholder='enter name'
-                                        name='user_name'
-                                    />
-                                    <TextField
-                                        style={textboxStyle}
-                                        fullWidth
-                                        variant='outlined'
-                                        label='subject'
-                                        // value={inputs}
-                                        // onChange={(e)=>setInputs(e.target.inputs)}
-                                        type='text'
-                                        placeholder='subject'
-                                        name='user_subject'
-                                    />
-                                    <TextField
-                                        style={textboxStyle}
-                                        fullWidth
-                                        variant='outlined'
-                                        label='email'
-                                        // value={inputs}
-                                        // onChange={(e)=>setInputs(e.target.inputs)}
-                                        type='text'
-                                        placeholder='enter email'
-                                        name='user_email'
-                                    />
-                                    <TextField
-                                        style={textboxStyle}
-                                        fullWidth
-                                        label='message'
-                                        variant='outlined'
-                                        multiline
-                                        rows={5}
-                                        // value={inputs}
-                                        // onChange={(e)=>setInputs(e.target.inputs)}
-                                        placeholder='Type message!'
-                                        name='message'
-                                    />
-                                    <Button 
-                                        variant='contained' 
-                                        type='submit' 
-                                        style={buttonStyle} 
-                                        fullWidth
-                                    >
-                                        Submit
-                                    </Button>
-                                    {sent && <p>Thank you!</p>}
-                                </form>
-                            </Paper>
-                        </Grid>
-                    </div> */}
-                    
-
                 </div>
             </div>
         </secion>
     )
 }
 
-const paperStyle = {padding: 20, height: '60vh', width: 400}
-const textboxStyle = {margin: '0.8rem 0'}
-const buttonStyle = {margin: '0.8rem auto', backgroundColor: '#ff8317', width: '50%'}
-
-export default Contact
+export default Contact;
